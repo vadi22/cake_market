b55698869bb2a3f84af49359d82f0d700e9f1754
Vadim <korovin555@.gmail.com>
2023-09-30 17:22:37 +0700

    Авторизация и регистрация сделана, config исправил

--- webapp/config.py
+++ webapp/config.py
@@ -1,12 +1,9 @@
-from dotenv import dotenv_values
 import os
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv()
-config = dotenv_values(".env")
-print(config)
-SQLALCHEMY_DATABASE_URI = 'postgresql://vnfswgnb:ypVRcYVSfjmUj_bdz4q4yeuAT7FMBRge@snuffleupagus.db.elephantsql.com/vnfswgnb' #не понимаю почему не могу достать, наверное из-за того что есть config.py
+SQLALCHEMY_DATABASE_URI = os.getenv("POSTGRESQL_DB") 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SECRET_KEY = 'MY_SECRET'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
