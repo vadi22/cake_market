2acee44e3384a033f3dc940b16a81a2c859d73a5
Andrey Samokhin <samokhin.andrey.s@gmail.com>
2023-11-03 07:29:25 +0100

    Поправил модели и корзину.

--- webapp/product/views.py
+++ webapp/product/views.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
 from flask_login import current_user
 
-from webapp.product.models import Product, Component, Image, Price, Labor, Product_Component, Product_Image, Component_Image
-
+from webapp.product.models import Product, Component, Image, Price, Labor, Product_Component
 blueprint = Blueprint('product', __name__, url_prefix='/product')
 
 @blueprint.route('/catalog')
@@ -21,9 +20,9 @@ def product_view(product_id):
     components_list = []
     product_cost = 0
     if product:
-        # Вытаскиваем фотографии продукта. Пока их всегда 1, но можно хранит несколько.
-        for image in product.product_images:
-            images_list.append(image.images)     
+        # # Вытаскиваем фотографии продукта. Пока их всегда 1, но можно хранит несколько.
+        # for image in product.product_images:
+        #     images_list.append(image.images)     
         # Считаем стоимость торта, получая все компоненты, их стоимость и скидку на них
         for components in product.product_component:
             for line in components.components.price:
@@ -36,8 +35,8 @@ def product_view(product_id):
         production_time = product.product_labor.hours
     return render_template(
         'product.html', 
-            product=product, 
-            images = images_list,
+            product = product, 
+            # images = images_list,
             product_cost = int(product_cost),
             production_time = int(production_time),
             components_list = components_list
