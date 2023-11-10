b28abfa53423ce05494fe1e4b50622cf1c75e798
Andrey Samokhin <samokhin.andrey.s@gmail.com>
2023-11-10 12:53:39 +0100

    Поправил корзину

--- webapp/order/views.py
+++ webapp/order/views.py
@@ -2,13 +2,13 @@ from flask import Blueprint, render_template, flash, redirect, url_for, request
 from flask_login import current_user
 from webapp.db import db
 from datetime import datetime
-from sqlalchemy import func, desc, asc
 
 from webapp.order.models import Order, Order_line, Line_status, Status
 from webapp.user.models import User
 from webapp.product.models import Product
 
-blueprint = Blueprint('order', __name__, url_prefix='/order')
+blueprint = Blueprint("order", __name__, url_prefix="/order")
+
 
 def summ_product_cost(product_id):
     product = Product.query.filter(Product.id == product_id).first_or_404()
@@ -18,7 +18,8 @@ def summ_product_cost(product_id):
             for line in components.components.price:
                 product_cost += line.cost * (1 - line.discount)
         product_cost += product.product_labor.cost
-    return(product_cost)
+    return product_cost
+
 
 def order_create(product_id):
     order = Order(user_id=current_user.id)
@@ -27,7 +28,9 @@ def order_create(product_id):
     # Считаем стоимость продукта
     product_cost = summ_product_cost(product_id)
     # После выполнения db.session.commit() в модели появляется id записи, его сразу же можго использовать: print(order.id)
-    product = Order_line(order_id=order.id, product_id=product_id, quantity=1, total_cost=product_cost)
+    product = Order_line(
+        order_id=order.id, product_id=product_id, quantity=1, total_cost=product_cost
+    )
     db.session.add(product)
     db.session.commit()
     # Устанавливаем статус на позицию, статус "В корзине"
@@ -35,39 +38,42 @@ def order_create(product_id):
     line_status = Line_status(line_id=order.id, datetime=datetime.now(), status_id=1)
     db.session.add(line_status)
     db.session.commit()
-    
-    flash(f'Заказ {order.id} создан')
+
+    flash(f"Заказ {order.id} создан")
     # После создания заказа переходим на страницу заказа.
     # Так как есть двойной вызов current_order, нужно подумать как лучше сделать.
     current_order = Order.query.join(User).filter(User.id == current_user.id).first()
-    return render_template('order.html', order=current_order)
+    return render_template("order.html", order=current_order)
 
-@blueprint.route('/orders', methods=['GET','POST'])
+
+@blueprint.route("/orders", methods=["GET", "POST"])
 def orders():
-    if request.method == 'GET':
+    if request.method == "GET":
         try:
             if current_user.is_admin:
                 orders = Order.query.all()
                 statuses = Status.query.all()
-                return render_template('orders.html',orders=orders, statuses=statuses)
+                return render_template("orders.html", orders=orders, statuses=statuses)
             else:
-                flash('Вы не авторизованы для данной операции')
-                return redirect(url_for('product.catalog'))
+                flash("Вы не авторизованы для данной операции")
+                return redirect(url_for("product.catalog"))
         except AttributeError:
-            flash('Вы не авторизованы для данной операции')
-            return redirect(url_for('product.catalog'))
-    if request.method == 'POST':
+            flash("Вы не авторизованы для данной операции")
+            return redirect(url_for("product.catalog"))
+    if request.method == "POST":
         try:
             if current_user.is_admin:
-        # Ищем все значения статусов и там где он выставлен новым (он числовой), 
-        # мы его меняем в базе данных 
+                # Ищем все значения статусов и там где он выставлен новым (он числовой),
+                # мы его меняем в базе данных
                 for key in request.form:
-                    if key.startswith('new_status.'):
-                        id_ = key.partition('.')[-1]
+                    if key.startswith("new_status."):
+                        id_ = key.partition(".")[-1]
                         try:
                             value = int(request.form[key])
-                            print(f'Для строки {id_} новый статус {value}')
-                            line_status = Line_status(line_id=id_, status_id=value, datetime=datetime.now())
+                            print(f"Для строки {id_} новый статус {value}")
+                            line_status = Line_status(
+                                line_id=id_, status_id=value, datetime=datetime.now()
+                            )
                             db.session.add(line_status)
                             db.session.commit()
                         except:
@@ -75,121 +81,120 @@ def orders():
                 # orders = Order.query.all()
                 # statuses = Status.query.all()
                 # return render_template('orders.html',orders=orders, statuses=statuses)
-                return redirect(url_for('order.orders'))
+                return redirect(url_for("order.orders"))
         except AttributeError:
-            flash('Вы не авторизованы для данной операции')
-            return redirect(url_for('product.catalog'))
-        
+            flash("Вы не авторизованы для данной операции")
+            return redirect(url_for("product.catalog"))
 
-@blueprint.route('/order' , methods=['GET','POST'])
+
+@blueprint.route("/order", methods=["GET", "POST"])
 def order_view():
-    if request.method == 'GET':
-    # Проверяем, есть ли заказ у текущего пользователя?
+    if request.method == "GET":
+        # Проверяем, есть ли заказ у текущего пользователя?
         try:
-            # current_order = Order.query \
-            #     .join(User) \
-            #     .join(Order_line) \
-            #     .join(Line_status) \
-            #     .filter(User.id == current_user.id, Line_status.status_id == 1) \
-            #     .order_by(desc(Line_status.datetime)) \
-            #     .first()     
-            current_order_sub = db.session.query(Line_status.line_id, func.max(Line_status.id)) \
-                    .group_by(Line_status.line_id) \
-                    .all()
-            order_max_id = [x[1] for x in current_order_sub]
-            order_sub = db.session.query(Line_status.line_id) \
-                    .filter(Line_status.id.in_(order_max_id), Line_status.status_id == 1) \
-                    .all()
-            order_sub = [x[0] for x in order_sub]
-            current_order = Order.query \
-                .join(User) \
-                .join(Order_line) \
-                .filter(User.id == current_user.id, Order_line.id.in_(order_sub)) \
+            current_order = (
+                Order.query.join(User)
+                .join(Order_line)
+                .join(Line_status)
+                .filter(User.id == current_user.id, Line_status.status_id == 1)
                 .first()
+            )
             # Если заказ есть, то пока просто отображаем страницу заказа
             if current_order:
-                return render_template('order.html', order=current_order)
-                # for order in current_order:
-                #     for line in order.line:
-                #         if line.line_status[0].status_id == 1:
-                #             return render_template('order.html', order=order)
-                # flash('Ваша корзина пуста, сначала добавьте товар в корзину')
-                # return redirect(url_for('product.catalog'))
-            # Если заказа нет, то создаем заказ, позицию заказа и статус позиции заказа = "В корзине"
+                return render_template("order.html", order=current_order)
+            # Если заказа нет, то просим добавить товар в корзину
             else:
-                flash('Ваша корзина пуста, сначала добавьте товар в корзину')
-                return redirect(url_for('product.catalog'))
+                flash("Ваша корзина пуста, сначала добавьте товар в корзину")
+                return redirect(url_for("product.catalog"))
         except AttributeError:
-            flash('Вы не авторизованы. Корзина доступна только авторизованным пользователям.')
-            return redirect(url_for('product.catalog'))
-    if request.method == 'POST':
+            flash(
+                "Вы не авторизованы. Корзина доступна только авторизованным пользователям."
+            )
+            return redirect(url_for("product.catalog"))
+    if request.method == "POST":
         try:
+            # Изменение количества в корзине
             for key in request.form:
-                if key.startswith('new_quantity.'):
-                    id_ = key.split('.')[-1]
-                    old_quantity = int(key.split('.')[-2])
+                if key.startswith("new_quantity."):
+                    id_ = key.split(".")[-1]
+                    old_quantity = int(key.split(".")[-2])
                     try:
                         value = int(request.form[key])
-                        print(f'Позиция {id_} было {old_quantity} стало {value}')
+                        # Если новое кол-во равно нулю, то количество в заказе снижаем до нуля, ставим статус 4 (удалено)
                         if value == 0:
-                            flash(f'Позиция удалена')
+                            flash(f"Позиция удалена")
                             order_line = db.session.query(Order_line).get(id_)
-                            order_line.quantity=value
+                            order_line.quantity = value
                             db.session.commit()
-                            line_status = Line_status(line_id=id_, datetime=datetime.now(), status_id=4)
+                            line_status = Line_status(
+                                line_id=id_, datetime=datetime.now(), status_id=4
+                            )
                             db.session.add(line_status)
                             db.session.commit()
-
+                        # Если новое кол-во просто отличается от старого, то меняем количество, статус не меняем.
                         elif old_quantity != value:
-                            flash(f'Изменено количество с {old_quantity} на {value}')
+                            flash(f"Заказ пересчитан")
                             order_line = db.session.query(Order_line).get(id_)
-                            order_line.quantity=value
+                            order_line.quantity = value
                             db.session.commit()
                     except:
                         pass
-            return redirect(url_for('order.order_view'))
+            return redirect(url_for("order.order_view"))
         except AttributeError:
-            flash('Вы не авторизованы для данной операции')
-            return redirect(url_for('product.catalog'))
+            flash("Вы не авторизованы для данной операции")
+            return redirect(url_for("product.catalog"))
 
 
-
-@blueprint.route('/<product_id>')
+@blueprint.route("/<product_id>")
 def order_change(product_id):
     try:
-        # Проверяем, есть ли заказ у текущего пользователя?
-        current_order = Order.query.join(User).filter(User.id == current_user.id).all()
         # Если заказ есть, то пока просто отображаем страницу заказа
+        current_order = (
+            Order.query.join(User)
+            .join(Order_line)
+            .join(Line_status)
+            .filter(User.id == current_user.id, Line_status.status_id == 1)
+            .first()
+        )
         if current_order:
-            for order in current_order:
-                # Ищу, есть ли заказ в корзине
-                if order.line[0].line_status[0].status_id == 1:
-                    # Если есть, то смотрим, есть ли в корзине выбранный товар
-                    product_in_order = Order_line.query.filter(Order_line.order_id == order.id, Order_line.product_id == product_id).first()
-                    # Если такой товар есть, то добавляю +1 к количеству
-                    if product_in_order:
-                        product_in_order.quantity += 1
-                        db.session.commit()
-                    # Если нет, то добавляем к заказу позицию с этим товаром
-                    else:
-                        product_cost = summ_product_cost(product_id)
-                        product_in_order = Order_line(order_id=order.id, product_id=product_id, quantity=1, total_cost=product_cost)
-                        db.session.add(product_in_order)
-                        db.session.commit()
-                    # Устанавливаем статус на позицию, статус "В корзине"
-                    # Решил что пусть будет лишний раз обновляться статус
-                    line_status = Line_status(line_id=product_in_order.id, datetime=datetime.now(), status_id=1)
-                    db.session.add(line_status)
-                    db.session.commit()
-                    flash(f'Товар добавлен в корзину')
-                    # После создания заказа переходим на страницу заказа.
-                    current_order = Order.query.join(User).filter(User.id == current_user.id).first()
-                    return render_template('order.html', order=current_order)
-            # Если нет заказа со статусом "В корзине", то создаем его 
-            order_create(product_id)
+            # Ищу, есть ли заказ в корзине
+            # Если есть, то смотрим, есть ли в корзине выбранный товар
+            product_in_order = Order_line.query.filter(
+                Order_line.order_id == current_order.id,
+                Order_line.product_id == product_id,
+            ).first()
+            # Если такой товар есть, то добавляю +1 к количеству
+            if product_in_order:
+                product_in_order.quantity += 1
+                db.session.commit()
+            # Если нет, то добавляем к заказу позицию с этим товаром
+            else:
+                product_cost = summ_product_cost(product_id)
+                product_in_order = Order_line(
+                    order_id=product_in_order.id,
+                    product_id=product_id,
+                    quantity=1,
+                    total_cost=product_cost,
+                )
+                db.session.add(product_in_order)
+                db.session.commit()
+            # Устанавливаем статус на позицию, статус "В корзине"
+            # Решил что пусть будет лишний раз обновляться статус
+            line_status = Line_status(
+                line_id=product_in_order.id,
+                datetime=datetime.now(),
+                status_id=1,
+            )
+            db.session.add(line_status)
+            db.session.commit()
+            flash(f"Товар добавлен в корзину")
+            # После создания заказа переходим на страницу заказа.
+            return redirect(url_for("order.order_view"))
         # Если заказа нет, то создаем заказ, позицию заказа и статус позиции заказа = "В корзине"
         else:
             order_create(product_id)
     except AttributeError:
-        flash('Вы не авторизованы. Добавить материал в корзину можно только после авторизации.')
-        return redirect(url_for('product.catalog'))
+        flash(
+            "Вы не авторизованы. Добавить материал в корзину можно только после авторизации."
+        )
+        return redirect(url_for("product.catalog"))
--- webapp/user/views.py
+++ webapp/user/views.py
@@ -5,43 +5,48 @@ from webapp.user.forms import LoginForm, RegistrationForm, AddressForm
 from webapp.user.models import User, User_adress
 from webapp.db import db
 
-blueprint = Blueprint('user', __name__, url_prefix='/users')
+blueprint = Blueprint("user", __name__, url_prefix="/users")
 
-@blueprint.route('/login')
+
+@blueprint.route("/login")
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     title = "Авторизация"
     login_form = LoginForm()
-    return render_template('login.html', page_title=title, form=login_form)
+    return render_template("login.html", page_title=title, form=login_form)
 
-@blueprint.route('/process-login', methods=['POST'])
+
+@blueprint.route("/process-login", methods=["POST"])
 def process_login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
-            flash('Вы вошли на сайт')
-            return redirect(url_for('index'))
-    flash('Неправильное имя пользователя или пароль')
-    return redirect(url_for('user.login'))
+            flash("Вы вошли на сайт")
+            return redirect(url_for("index"))
+    flash("Неправильное имя пользователя или пароль")
+    return redirect(url_for("user.login"))
 
-@blueprint.route('/logout')
+
+@blueprint.route("/logout")
 def logout():
     logout_user()
-    flash('Вы успешно разлогинились')
-    return redirect(url_for('index'))
+    flash("Вы успешно разлогинились")
+    return redirect(url_for("index"))
 
-@blueprint.route('/register')
+
+@blueprint.route("/register")
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     form = RegistrationForm()
     title = "Регистрация"
-    return render_template('registration.html', page_title=title, form=form)
+    return render_template("registration.html", page_title=title, form=form)
 
-@blueprint.route('/process-reg', methods=['POST'])
+
+@blueprint.route("/process-reg", methods=["POST"])
 def process_reg():
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -49,46 +54,68 @@ def process_reg():
         new_user.set_password(form.password.data)
         db.session.add(new_user)
         db.session.commit()
-        flash('Вы успешно зарегистрировались!')
-        return redirect(url_for('user.login'))
+        flash("Вы успешно зарегистрировались!")
+        return redirect(url_for("user.login"))
     else:
         for field, errors in form.errors.items():
             for error in errors:
-                flash('Ошибка в поле "{}": - {}'.format(getattr(form, field).label.text, error))
-        return redirect(url_for('user.register'))
+                flash(
+                    'Ошибка в поле "{}": - {}'.format(
+                        getattr(form, field).label.text, error
+                    )
+                )
+        return redirect(url_for("user.register"))
 
-@blueprint.route('/<int:user_id>')
-@login_required
+
+@blueprint.route("/<int:user_id>")
+# @login_required
 def user_profile(user_id):
     user = User.query.filter(User.id == user_id).first_or_404()
     adress = User_adress.query.filter(User_adress.user_id == user_id).first()
-    if current_user != user:
-        abort(404)
-    return render_template(
-        'user_profile.html', 
-            user=user, 
-            page_title = 'Личный кабинет',
-            adress = adress,
-    )
+    print(current_user.id)
+    print(current_user.is_admin)
+    print(current_user == user)
+    if current_user.is_admin or current_user == user:
+        return render_template(
+            "user_profile.html",
+            user=user,
+            page_title="Личный кабинет",
+            adress=adress,
+        )
+    else:
+        flash("Вы не авторизованы для данной операции")
+        # Тут можно настроить возврат к предыдущему окну, а не на главную.
+        return redirect(url_for("index"))
 
-@blueprint.route('/<int:user_id>/edit_profile', methods=['GET'])
+
+@blueprint.route("/<int:user_id>/edit_profile", methods=["GET"])
 @login_required
 def edit_profile(user_id):
     user = User.query.filter(User.id == user_id).first_or_404()
     if current_user != user:
         abort(404)
     form = AddressForm()
-    return render_template('edit_profile.html', form=form, )
+    return render_template(
+        "edit_profile.html",
+        form=form,
+    )
 
-@blueprint.route('/edit_adress', methods=['GET', 'POST'])
+
+@blueprint.route("/edit_adress", methods=["GET", "POST"])
 def edit_adress():
     form = AddressForm()
     adress = User_adress.query.filter(User_adress.user_id == current_user.id).first()
     if adress:
         db.session.delete(adress)
-    adress = User_adress(city=form.city.data, district=form.district.data, street=form.street.data, home=form.home.data, apartment=form.apartment.data, user_id=current_user.id)
+    adress = User_adress(
+        city=form.city.data,
+        district=form.district.data,
+        street=form.street.data,
+        home=form.home.data,
+        apartment=form.apartment.data,
+        user_id=current_user.id,
+    )
     db.session.add(adress)
     db.session.commit()
-    flash('Адрес принят')
-    return redirect(url_for('user.user_profile', user_id=current_user.id))
-    
+    flash("Адрес принят")
+    return redirect(url_for("user.user_profile", user_id=current_user.id))
